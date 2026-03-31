993d759fed04cab4a88daa173ae08acf364c5cca
WhiteTafal <givijf@gmail.com>
2026-03-31 21:16:34 +0400

    fix footer: fix name and alignment

--- theme/blocks/footer.php
+++ theme/blocks/footer.php
@@ -2,7 +2,7 @@
 	<div class="container">
         <div class="authors">
         	<div class="copyright">© 2015 შპს ლიტერატურა</div>
-            <div class="developer"><a href="https://whitefatal.github.io/portfolio/" target=_blank>Givi Jamburia</a></div>
+            <div class="developer"><a href="https://whitefatal.github.io/portfolio/" target=_blank>გივი ჯამბურია | Givi Jamburia</a></div>
             <div class="line">|</div>
             <div class="developer">დეველოპერები: <a href="mailto:enji777@gmail.com">ნიკა ჯანგველაძე</a></div>
             <div class="line">|</div>
