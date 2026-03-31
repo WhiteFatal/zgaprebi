df279dfc16032d88e5f8beedfa7f9499d7f0a169
WhiteTafal <givijf@gmail.com>
2026-03-31 21:11:27 +0400

    fix footer: add my name

--- theme/blocks/footer.php
+++ theme/blocks/footer.php
@@ -2,11 +2,11 @@
 	<div class="container">
         <div class="authors">
         	<div class="copyright">© 2015 შპს ლიტერატურა</div>
-            <div class="developer">დეველოპერები: <a href="mailto:enji777@gmail.com">ნიკა ჯანგველაძე</a>
+            <div class="developer">დეველოპერები: <a href="mailto:enji777@gmail.com">ნიკა ჯანგველაძე</a></div>
             <div class="line">|</div>
-            <a href="https://whitefatal.github.io/portfolio/ target=_blank">Givi Jamburia</a></div>
+            <div class="developer"><a href="https://whitefatal.github.io/portfolio/" target=_blank>Givi Jamburia</a></div>
             <div class="line">|</div>
-            <div class="designer">დიზაინერი: <a href="http://legadesign.ge/ target=_blank">გიორგი ლევერაშვილი</a></div>
+            <div class="designer">დიზაინერი: <a href="http://legadesign.ge/" target=_blank>გიორგი ლევერაშვილი</a></div>
             <div class="clb"></div>
         </div>
     </div>
